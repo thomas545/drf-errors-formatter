f92bd77b6c217b10aa3671a882be9530d2eca4c9
thomas434 <tomas.temo77@gmail.com>
2021-08-05 10:34:03 +0200

    add version.py

---
+++ drf_handlers/__version__.py
@@ -0,0 +1,8 @@
+__title__ = 'drf-errors-formatter'
+__description__ = 'Format errors in Django Rest Framework.'
+__url__ = 'https://github.com/thomas545/drf-errors-formatter'
+__version__ = '0.1.0'
+__author__ = '@thomas545 https://github.com/thomas545'
+__author_email__ = 'thomas.adel31@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021 @thomas545 https://github.com/thomas545'
